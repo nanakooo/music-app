98d3a01b50b1fc39968602e3fb316a492825a47b
nanako <879296398@qq.com>
2020-11-13 16:41:38 +0800

    4

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,14 +1,14 @@
 <template>
 	<view>
-	<u-search placeholder="搜索歌曲" v-model="keyword" margin="10rpx 16rpx"></u-search>
-	<view class="wrap">
+		<u-search placeholder="搜索歌曲" v-model="keyword" margin="10rpx 16rpx"></u-search>
+		<view class="wrap">
 			<swiper indicator-dots circular>
 				<swiper-item v-for="item in swipers">
 					<image :src="item.imageUrl"></image>
 				</swiper-item>
 			</swiper>
 		</view>
-	<u-grid :col="4">
+		<u-grid :col="4">
 			<u-grid-item>
 				<u-badge count="9" :offset="[20, 20]"></u-badge>
 				<u-icon name="calendar" :size="46"></u-icon>
@@ -28,25 +28,31 @@
 				<view class="grid-text">数字专辑</view>
 			</u-grid-item>
 		</u-grid>
-	<view class="youLike">
-		<h2>猜你喜欢</h2>
-				<scroll-view scroll-x="true">
-					<view class="item1">
-						
-					</view>
-					<view class="item2">
-						
-					</view>
-					<view class="item3">
-						
-					</view>
-				</scroll-view>
+		<view class="youLike">
+			<h2>猜你喜欢</h2>
+			<scroll-view scroll-x="true">
+				<view class="item1">
 
-	</view>
-	<view class="musicMenu">
-		<h2>精选歌单</h2>
-	</view>
+				</view>
+				<view class="item2">
+
+				</view>
+				<view class="item3">
+
+				</view>
+			</scroll-view>
+
 		</view>
+		<view class="musicMenu">
+			<h2>精选歌单</h2>
+			<view class="menuBox">
+				<view class="someSong" v-for="item in lists">
+					<image :src="item.picUrl"></image>
+					<span>{{item.name}}</span>
+				</view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
@@ -55,37 +61,54 @@
 			return {
 				keyword: '稻香',
 				swipers: [],
+				lists: []
 			}
 		},
 		onLoad() {
-			this.getSwipers()
+			this.getSwipers(),
+				this.getLists()
 		},
 		methods: {
 			/*获取轮播图数据*/
 			getSwipers() {
 				console.log("获取数据")
 				uni.request({
-					url:'http://localhost:3000/banner',
-					success:res=>{
+					url: 'http://localhost:3000/banner',
+					success: res => {
 						console.log(res)
-						if(res.data.code !==200) {
+						if (res.data.code !== 200) {
 							return uni.showToast({
-								title:"获取数据失败"
+								title: "获取数据失败"
 							})
 						}
 						this.swipers = res.data.banners;
 					}
 				})
 			},
-			newSong(){
+			getLists() {
+				console.log("获取数据")
+				uni.request({
+					url: 'http://localhost:3000/personalized?limit=6',
+					success: res => {
+						console.log(res)
+						if (res.data.code !== 200) {
+							return uni.showToast({
+								title: "获取数据失败"
+							})
+						}
+						this.lists = res.data.result;
+					}
+				})
+			},
+			newSong() {
 				uni.navigateTo({
-				       url: '/pages/newSong/newSong'
+					url: '/pages/newSong/newSong'
 				})
 				console.log("新歌速递")
 			},
-			level(){
+			level() {
 				uni.navigateTo({
-				       url: '/pages/level/level'
+					url: '/pages/level/level'
 				})
 				console.log("排行榜")
 			}
@@ -95,63 +118,86 @@
 
 <style lang="scss" scoped>
 	.wrap {
-		swiper{
+		swiper {
 			width: 750rpx;
 			height: 350rpx;
-			image{
-				width:100%;
-				height:100%;
+
+			image {
+				width: 100%;
+				height: 100%;
 			}
 		}
 	}
-	.badge-icon {
-			position: absolute;
-			top: 14rpx;
-			right: 40rpx;
-			width: 30rpx;
-			height: 30rpx;
-		}
-		
-		.grid-text {
-			font-size: 28rpx;
-			margin-top: 4rpx;
-			color: $u-type-info;
-		}
-		.youLike{
-			padding:12rpx 35rpx;
-			background-color: #FEF0F0;
-		}
-		.youLike >h2{
-			color:darkred;
-			
-		}
-		.musicMenu >h2{
-			color:darkred;
-		}
-		.musicMenu{
-			padding:12rpx 35rpx;
-			
-		}
-	scroll-view{
-			width: 80%;
-			padding:10rpx 50rpx;
-			height: 280rpx;
-			white-space: nowrap;
-		}
-		.item1,.item2,.item3{
-			display: inline-block;
-			width: 100%;
-			height: 100%;
-		}
-		.item1{
-			background-color: white;
-		}
-		.item2{
-			background-color: white;
-		}
-		.item3{
-			background-color: white;
-		}
-		
 
+	.badge-icon {
+		position: absolute;
+		top: 14rpx;
+		right: 40rpx;
+		width: 30rpx;
+		height: 30rpx;
+	}
+
+	.grid-text {
+		font-size: 28rpx;
+		margin-top: 4rpx;
+		color: $u-type-info;
+	}
+
+	.youLike {
+		padding: 12rpx 35rpx;
+		background-color: #FEF0F0;
+	}
+
+	.youLike>h2 {
+		color: darkred;
+
+	}
+
+	.musicMenu>h2 {
+		color: darkred;
+	}
+
+	.musicMenu {
+		padding: 12rpx 35rpx;
+
+	}
+
+	scroll-view {
+		width: 80%;
+		padding: 10rpx 50rpx;
+		height: 280rpx;
+		white-space: nowrap;
+	}
+
+	.item1,
+	.item2,
+	.item3 {
+		display: inline-block;
+		width: 100%;
+		height: 100%;
+	}
+
+	.item1 {
+		background-color: white;
+	}
+
+	.item2 {
+		background-color: white;
+	}
+
+	.item3 {
+		background-color: white;
+	}
+	.menuBox{
+		padding-left:20rpx;
+	.someSong{
+		width:33%;
+		float:left;
+		padding:15rpx 5rpx;
+		image{
+			width:100%;
+			height:200rpx;
+		}
+	  }
+	}
 </style>
--- pages/newSong/newSong.vue
+++ pages/newSong/newSong.vue
@@ -1,8 +1,50 @@
+
 <template>
+        <view>
+                <view class="inv-h-w">
+                        <view :class="['inv-h',Inv==0?'inv-h-se':'']" @click="Inv=0">全部</view>
+                        <view :class="['inv-h',Inv==1?'inv-h-se':'']" @click="Inv=1">华语</view>
+						<view :class="['inv-h',Inv==2?'inv-h-se':'']" @click="Inv=2">欧美</view>
+						<view :class="['inv-h',Inv==3?'inv-h-se':'']" @click="Inv=3">日本</view>
+						<view :class="['inv-h',Inv==4?'inv-h-se':'']" @click="Inv=4">韩国</view>
+                </view>
+                <view class="" v-show="Inv == 0">
+                        全部
+                </view>
+                <view class="" v-show="Inv == 1">
+                        华语
+                </view>
+				<view class="" v-show="Inv == 2">
+				        欧美
+				</view>
+				<view class="" v-show="Inv == 3">
+				        日本
+				</view>
+				<view class="" v-show="Inv == 4">
+				        韩国
+				</view>
+        </view>
 </template>
-
+ 
 <script>
+        export default {
+                data() {
+                        return {
+                                Inv:0
+                        }
+                },
+                methods: {
+                        changeTab(Inv){
+                                that.navIdx = Inv;
+                                 
+                        },
+                }
+        }
 </script>
-
+         
 <style>
+        .inv-h-w{background-color: #FFFFFF;height: 100upx;display: flex;}
+        .inv-h{font-size: 30upx;flex: 1;text-align: center;color: #C9C9C9;height: 100upx;line-height: 100upx;}
+        .inv-h-se{color: #5BA7FF;border-bottom: 4upx solid #5BA7FF;}
+        page{background-color: #F2F2F2;}
 </style>
